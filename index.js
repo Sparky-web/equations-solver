9839034231ad89e45d7ffcd2e8312c6e8680429a
Vlad BABINOV <vladbabinov@vlad.local>
2021-09-29 14:38:14 +0500

    big integers support

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const solve = (a = 10, b = 200, c = 20, d = 15, e = 3) => {
     const equation = `(${a}-(${b}/(${c}+x+${d})))-(${a}-(${b}/(${c}+x)))=${e}`
 
     // returns array
-    return JSON.parse(nerdamer.solve(equation, "x").toString())
+    const solved = nerdamer.solve(equation, "x")
+    const arr = solved.symbol.elements.map(e => BigInt(e.multiplier.num.toString()))
+
+    return arr
 }
 
 
-console.log(solve())
+console.log(solve(112477.754, 153262.677 * 112477.754, 153262.677, 1200, 800.0055186911923))
